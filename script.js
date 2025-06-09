90842b8c9521177280d45a6b12f272a110faea94
Denise Silva <denisekellypereira@hotmail.com>
2025-06-09 12:20:42 -0300

    Dispensa do Nam e Tae

--- script.js
+++ script.js
@@ -1,45 +1,3 @@
-
-
-function countdown3() {
-    const targetDate2 = new Date("2025-06-10T00:00:00+09:00"); // Data-alvo para o Evento 2
-    
-    const now = new Date().getTime();
-    const difference = targetDate2 - now;
-    
-    if (difference < 0) {
-        clearInterval(interval2);
-        document.getElementById('countdown3').innerHTML = "Ele saiu!!!!";
-        return;
-    }
-    
-    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-    
-    document.getElementById('countdown3').innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-}
-
-function countdown4() {
-    const targetDate2 = new Date("2025-06-10T00:00:00+09:00"); // Data-alvo para o Evento 2
-    
-    const now = new Date().getTime();
-    const difference = targetDate2 - now;
-    
-    if (difference < 0) {
-        clearInterval(interval2);
-        document.getElementById('countdown4').innerHTML = "Ele saiu!!!!";
-        return;
-    }
-    
-    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-    
-    document.getElementById('countdown4').innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-}
-
 function countdown5() {
     const targetDate2 = new Date("2025-06-11T00:00:00+09:00"); // Data-alvo para o Evento 2
     
@@ -101,11 +59,7 @@ function countdown7() {
 }
 
 
-countdown3(); // Inicia a contagem regressiva para o Evento 2 imediatamente
-const interval3 = setInterval(countdown3, 1000); // Atualiza a contagem regressiva para o Evento 2 a cada segundo
 
-countdown4(); // Inicia a contagem regressiva para o Evento 2 imediatamente
-const interval4 = setInterval(countdown4, 1000); // Atualiza a contagem regressiva para o Evento 2 a cada segundo
 
 countdown5(); // Inicia a contagem regressiva para o Evento 2 imediatamente
 const interval5 = setInterval(countdown5, 1000); // Atualiza a contagem regressiva para o Evento 2 a cada segundo
